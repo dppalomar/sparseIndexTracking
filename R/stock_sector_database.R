3fb4c950153c3217629ebb8e10d8e220367dfdf1
Daniel P. Palomar <daniel.p.palomar@gmail.com>
2018-04-25 14:17:32 +0800

    Vignette fixes

---
+++ R/stock_sector_database.R
@@ -0,0 +1,17 @@
+#' Database of the sectors corresponding to the S&P 500 stocks
+#'
+#' @docType data
+#'
+#' @usage data(stock_sector_database)
+#'
+#' @format A matrix with first column stock symbols and second column stock sector
+#' \describe{
+#'   \item{symbol}{stock symbol}
+#'   \item{sector}{sector of the stocks}
+#' }
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(stock_sector_database)
+"stock_sector_database"
