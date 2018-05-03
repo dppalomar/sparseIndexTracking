d51c287b92fbe3e31e6225ad2c723091dfb7c30f
Daniel P. Palomar <daniel.p.palomar@gmail.com>
2018-05-03 15:34:50 +0800

    Nice plotting of results now since they are xts

--- R_buildignore/example.R
+++ R_buildignore/example.R
@@ -1,39 +1,29 @@
-source('spIndexTrack.R')
+#source('spIndexTrack.R')
 library(xts)
+library(sparseIndexTracking)
 
 # parameters
-n <- 500 # number of assets
-m <- 200 # days
+N <- 500  # number of assets
+T <- 200  # days
 
 # artificial data
-mu <- rnorm(n) / 252
-D <- 2 * diag(runif(n)) / sqrt(252)
+mu <- rnorm(N) / 252
+D <- 2 * diag(runif(N)) / sqrt(252)
 
-C <- matrix(rep(0, n*n), nrow = n)
-for (i in 1:n) {
-  for (j in 1:n) {
+C <- matrix(rep(0, N^2), nrow = N)
+for (i in 1:N)
+  for (j in 1:N)
     C[i, j] <- 0.7^(abs(i-j))
-  }
-}
 
 Sigma <- D %*% C %*% D
 
 # asset returns
-X <- MASS::mvrnorm(n = m, mu, Sigma)
+X <- MASS::mvrnorm(n = T, mu, Sigma)
 
 # index
-t <- runif(n)
-w_ind <- t/sum(t)
+w_ind <- runif(N)
+w_ind <- w_ind/sum(w_ind)
 r_b <- X %*% w_ind
-
-# load('data_2010_2015.RData')
-#
-# colNames <- colnames(lin_returns)
-# Xc <- coredata(lin_returns)
-# Xdf <- as.data.frame(lin_returns)
-# X <- as.matrix(lin_returns)
-#
-# r_b <- coredata(SP500)
 
 # algorithm
 lambda <- 5e-7
@@ -41,6 +31,36 @@ w <- spIndexTrack(X, r_b, lambda, u = 0.5, measure = 'ete', hub = 0.05)
 
 print(sum(w > 1e-6))
 
-matplot(seq(m), cbind(cumprod(1 + X %*% w), cumprod(1 + r_b)), type = 'l', xlab = 'Days', ylab = 'Wealth')
+matplot(seq(T), cbind(cumprod(1 + X %*% w), cumprod(1 + r_b)), type = 'l', xlab = 'Days', ylab = 'Wealth')
 legend("top", c('Portfolio', 'Index'), col = seq_len(2), cex=0.8, fill = seq_len(2))
 grid()
+
+###########################################
+
+load('R_buildignore/data_2010_2015.RData')
+# colNames <- colnames(lin_returns)
+# Xc <- coredata(lin_returns)
+# Xdf <- as.data.frame(lin_returns)
+# X <- as.matrix(lin_returns)
+# r_b <- coredata(SP500)
+X <- lin_returns
+N <- ncol(X)
+T <- nrow(X)
+
+
+cbind("portfolio" = X %*% w, SP500)
+
+
+# algorithm
+w <- spIndexTrack(X, r = SP500, lambda = 5e-7, u = 0.5, measure = 'ete', hub = 0.05)
+
+print(sum(w > 1e-6))
+
+# ugly plot without xts
+matplot(seq(T), cbind(cumprod(1 + X %*% w), cumprod(1 + SP500)), type = 'l', xlab = 'Days', ylab = 'Wealth')
+legend("top", c('Portfolio', 'Index'), col = seq_len(2), cex=0.8, fill = seq_len(2))
+grid()
+
+# nice plot with xts
+plot(cbind("portfolio" = cumprod(1 + X %*% w), cumprod(1 + SP500)), legend.loc = "topleft", main = "Cumulative P&L")
+
