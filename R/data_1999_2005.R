6331f841ed390110327111278a9d8fe36c8411ff
Daniel P. Palomar <daniel.p.palomar@gmail.com>
2018-05-15 18:43:04 +0800

    Revision manual pages

--- R/data_1999_2005.R
+++ R/data_1999_2005.R
@@ -1,13 +1,13 @@
-#' Database of the net returns of the index S&P 500 and its underlying assets
+#' Database of the net returns of the index S&P 500 and its underlying assets during 7/1999 - 12/2005
 #'
 #' Database of the net returns of the index S&P 500 and its underlying assets during the period 7/1999 - 12/2005.
-#' Only the assets that where in the index during the whole period are included.
+#' Only the assets that were in the index during the whole period are included.
 #'
 #' @docType data
 #'
 #' @usage data(data_1999_2005)
 #'
-#' @format A list of two xts objects, namely 'X' and 'SP500', corresponding to the net returns of the assets and the index, respectively.
+#' @format A list of two xts objects, namely, \code{X} and \code{SP500}, corresponding to the net returns of the assets and the index, respectively.
 #'
 #' @keywords datasets
 #'
