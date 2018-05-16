c877d7448520af6c8544812703f40c4a239fb2be
Daniel P. Palomar <daniel.p.palomar@gmail.com>
2018-05-16 09:50:03 +0800

    Remove highres figures and revise roi comparison.

--- R_buildignore/roi_comparison.R
+++ R_buildignore/roi_comparison.R
@@ -27,8 +27,8 @@ Lc[1, 1:num_assets] <- 1  # sum(w) = 1
 Lc[2:(num_assets+1), 1:num_assets] <- diag(num_assets) # w <= u*s
 Lc[2:(num_assets+1), (num_assets+1):(2*num_assets)] <- -u * diag(num_assets)
 
+dir <- c("==", rep("<=", num_assets))
 rhs <- c(1, rep.int(0, num_assets))
-dir <- c("=", rep("<=", num_assets))
 prob <- OP(objective = Q_objective(Q, Lobj),
            constraints = L_constraint(L = Lc,
                                       dir = dir,
@@ -36,12 +36,40 @@ prob <- OP(objective = Q_objective(Q, Lobj),
            bounds = V_bound(li = 1:(2*num_assets),
                             ui = 1:(2*num_assets),
                             lb = rep(0, 2*num_assets),
-                            ub = c(rep(u, num_assets), rep(1, num_assets))
-                            ),
+                            ub = c(rep(u, num_assets), rep(1, num_assets))),
            types =  c(rep("C", num_assets), rep("B", num_assets))
            )
 
+########################################################
+# Solvers available for this problem:
+# [1] "ROI.plugin.cplex"  "ROI.plugin.cplex"  "ROI.plugin.gurobi" "ROI.plugin.mosek"  "ROI.plugin.neos"
+# [6] "ROI.plugin.gurobi" "ROI.plugin.mosek"
+ROI_available_solvers(prob)$Package
 
- res <- ROI_solve(prob)
+# Solvers installed (not necessarily loaded):
+ROI_installed_solvers()
+
+#trying to install ROI.plugin.gurobi, but I don't have gurobi (required)
+#install.packages("ROI.plugin.gurobi")
+#devtools::install_github('kuol/ROI.plugin.gurobi')
+#library(ROI.plugin.gurobi)
+
+#trying to install ROI.plugin.cplex, but I don't have cplex (required)
+#install.packages("ROI.plugin.cplex")
+
+#trying to install ROI.plugin.mosek, but I don't have Rmosek (required)
+#install.packages("ROI.plugin.mosek")
+#devtools::install_github("Fl0Sch/ROI.plugin.mosek")
+
+
+# Solvers installed and loaded (registered) in system:
+ROI_registered_solvers()
+
+# Solvers installed and loaded in system applicable to the particular problem:
+ROI_applicable_solvers(prob)
+########################################################
+
+
+res <- ROI_solve(prob)
 
 
