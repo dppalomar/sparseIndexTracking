dcf62b72c6b69f70eb78b40e8bf7739e183e01c7
Daniel P. Palomar <daniel.p.palomar@gmail.com>
2018-12-22 22:59:45 +0800

    Small updates

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,3 @@
 # sparseIndexTracking 0.1.0
 
-* Initial release.
+* Initial release version 0.1.0 is on CRAN.
--- R_buildignore/developer_commands.R
+++ R_buildignore/developer_commands.R
@@ -18,53 +18,31 @@ package?sparseIndexTracking
 ##
 ## Developer commands (http://r-pkgs.had.co.nz/)
 ##
-library(devtools)
-#devtools::create("sparseIndexTracking")
 devtools::load_all()  #or Ctrl-Shift-L
-#devtools::use_package("mvtnorm")
 devtools::install()
-#devtools::build()  # to generate the installation file
-#devtools::use_readme_rmd()  # to create the README file
-#devtools::use_data_raw()  # to set up the raw-data folder
-
+library(sparseIndexTracking)
 
 # Documentation
-devtools::document()  #to generate all documentation via roxygen
+devtools::document()  # to generate all documentation via roxygen
 ?spIndexTrack
 ?INDEX_2010
-
-# README (.md file has to be generated manually by clicking Knitr)
-
-# Vignettes
-#devtools::use_vignette("SparseIndexTracking")  # to create the folder the first time
-#rmarkdown::render("vignettes/SparseIndexTracking-vignette.Rmd", "md_document")  # this is to generate the .md for GitHub
-#rmarkdown::render("vignettes/SparseIndexTracking-vignette.Rmd", "bookdown::html_document2")
-#rmarkdown::render("vignettes/SparseIndexTracking-vignette.Rmd", "bookdown::pdf_document2")
-#tools::compactPDF("vignettes/SparseIndexTracking-vignette.pdf", gs_quality = "ebook")
-#browseVignettes("sparseIndexTracking")
-
-
-
-# Code style
-lintr::lint_package()
 
 
 # Code tests
 #devtools::use_testthat()  # the first time
 devtools::test()
-covr::package_coverage()  #coverage of tests
+#covr::package_coverage()  # coverage of tests
 #goodpractice::gp()  # overall checks
 
 
-# CRAN check and submission
+# CRAN check and submission (http://r-pkgs.had.co.nz/release.html)
+#  checklist: https://kalimu.github.io/post/checklist-for-r-package-submission-to-cran/
 devtools::check()
 rcmdcheck::rcmdcheck()
 devtools::build()
-#R CMD build . --compact-vignettes=gs+qpdf  # this is to generate tarball
+#devtools::build_win()  #to check under windows
+#R CMD build .  # this is to generate tarball
+#R CMD build . --compact-vignettes=gs+qpdf
 #R CMD check sparseIndexTracking_0.1.0.tar.gz --as-cran  # this is before submission to CRAN
 #submit the tarball directly via the webform: https://cran.r-project.org/submit.html
-
-# An alternative is to upload to CRAN via devtools:
-#devtools::build_win()  #to check under windows
-devtools::release(args = "--compact-vignettes=gs+qpdf")  #for CRAN
 
